9d7c8215c33c850b881beb112cad4687cb1710a6
lichenghu <lichenghu@xt-school.com>
2020-05-29 14:05:54 +0800

    feat: 配置及编译统一

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import glob from 'glob';
+import chokidar from 'chokidar';
 import {IPluginContext} from '@tarojs/service';
 import winPath from "./utils/winPath";
 
@@ -10,19 +11,62 @@ const getPageFiles = (sourcePath: string) => {
   });
 }
 
+const buildTempPages = (sourcePath: string, chalk: any) => {
+  const pagesFileName = `${sourcePath}/.temp/pages.js`;
+  const pages = getPageFiles(sourcePath);
+  const tpl = `module.exports = ${JSON.stringify(pages, null, 2)};`;
+  fs.writeFileSync(pagesFileName, tpl);
+  console.log(`${chalk.blue('生成')}  配置文件  ${pagesFileName}`);
+}
+
+const checkPage = (path: string) => {
+  return /(.*)\/index\.(vue|tsx?|jsx?)$/.test(winPath(path));
+}
+
+const watchPagesPath = (sourcePath: string, chalk: any) => {
+  const watch = chokidar.watch(`${sourcePath}/pages/`).on('all', (event, path) => {
+    let type;
+    switch(event){
+      case "add":
+      case "unlink":
+        if(checkPage(path)){
+          type = event;
+        }
+        break;
+      default:
+    }
+    if(type){
+      console.log(`${chalk.blue(type)}  page  ${winPath(path)}`);
+      buildTempPages(sourcePath, chalk);
+    }
+  });
+
+  return () => watch.close();
+}
+
 export default (ctx: IPluginContext) => {
+  const sourcePath = winPath(ctx.paths.sourcePath);
+  const chalk = ctx.helper.chalk;
+  const appJsonFileName = 'app.json';
+
   ctx.onBuildStart(() => {
-    const sourcePath = winPath(ctx.paths.sourcePath);
     const tmpPath = `${sourcePath}/.temp`;
     if(!fs.existsSync(tmpPath)){
       fs.mkdirSync(tmpPath);
     }
 
-    const pagesFileName = `${tmpPath}/pages.js`;
+    watchPagesPath(sourcePath, chalk);
+  });
+
+  ctx.modifyBuildAssets((args: any) => {
+    const appSource = args.assets[appJsonFileName].source();
     const pages = getPageFiles(sourcePath);
-    const tpl = `module.exports = ${JSON.stringify(pages, null, 2)};`;
-    fs.writeFileSync(pagesFileName, tpl);
-    const chalk = ctx.helper.chalk;
-    console.log(`${chalk.blue('生成')}  配置文件  ${pagesFileName}`);
+
+    const appJson = JSON.parse(appSource);
+    appJson.pages = pages;
+
+    args.assets[appJsonFileName].source = () => {
+      return JSON.stringify(appJson);
+    };
   });
 };
